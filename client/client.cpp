1ce00f8f105e92ce4681f17adfb2f2b38b7b44f7
yigh <yizhou12345@gmail.com>
2015-04-14 17:45:30 -0500

    passing reg messages

--- client/client.cpp
+++ client/client.cpp
@@ -27,17 +27,13 @@ int main (int argc, char *argv[])
     zmq::socket_t requester(context, ZMQ_REQ);
     requester.connect("tcp://localhost:5559");
 
-    for( int request = 0 ; request < 1 ; request++) {
+    zmq::message_t msg_req (req_serialized.size());
+    memcpy ((void *) msg_req.data(),
+                    req_serialized.c_str(),
+                    req_serialized.size());
 
-        zmq::message_t msg_req (req_serialized.size());
-        memcpy ((void *) msg_req.data(),
-                        req_serialized.c_str(),
-                        req_serialized.size());
-
-        requester.send(msg_req);
-        //s_send (requester, "Hello");
-        //std::string string = s_recv (requester);
-        //std::cout << "Received reply " << request
-        //        << " [" << string << "]" << std::endl;
-    }
+    requester.send(msg_req);
+    //s_send (requester, "Hello");
+    std::string string = s_recv (requester);
+    std::cout << "Received reply [" << string << "]" << std::endl;
 }
--- server/src/worker.cpp
+++ server/src/worker.cpp
@@ -1,4 +1,7 @@
 #include "includes/zhelpers.hpp"
+#include "includes/social.pb.cc"
+
+#include <iostream>
 
 int main (int argc, char *argv[])
 {
@@ -10,15 +13,27 @@ int main (int argc, char *argv[])
     while(1)
     {
         //  Wait for next request from client
-        std::string string = s_recv (responder);
+        // std::string string = s_recv (responder);
+        zmq::message_t reply;
+        responder.recv (&reply);
+
+        netmsg::AppRequest rcv_msg;
+        rcv_msg.ParseFromArray(reply.data(), reply.size());
+
+        std::cout << rcv_msg.msg_type() << std::endl;
+
+        netmsg::AppRequest_RegisterMessage reg_msg = rcv_msg.reg_msg();
+        std::cout << reg_msg.username() << "  " <<
+                     reg_msg.email() << "  " <<
+                     reg_msg.secured_password() << std::endl;
         
-        std::cout << "Received request: " << string << std::endl;
+        // std::cout << "Received request: " << string << std::endl;
         
         // Do some 'work'
-        sleep (1);
+        // sleep (1);
         
         //  Send reply back to client
-        s_send (responder, "World");
+         s_send (responder, "1");
         
     }
 }
